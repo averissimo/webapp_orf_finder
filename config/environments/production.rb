7ffc36f316b071b38bca8bd3a3ca9bf0e07cab2a
André Veríssimo <afsverissimo@gmail.com>
2017-02-23 04:08:58 +0000

    own configurations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # config.relative_url_root = '/orf_finder'
 end
